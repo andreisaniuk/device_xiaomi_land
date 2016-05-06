5562f14b6a45da091306d05ed2069ab7f8f7e1b2
Sampath Vangaveti <sampathv@codeaurora.org>
2016-05-06 16:09:12 +0530

    QCamera2: HAL: Convert Boottime from ISP to MONOTONIC
    Issue:
    Diplay freeze observed sometimes, if there is any
    suspend/resume of the device.
    The issue found to be with differences in the timesources
    used by display and camera for timestamp calculation.
    
    Fix:
    Measure the clock offset between BOOTTIME and MONOTONIC.
    The clock domain source for ISP is BOOTTIME and
    for display, it is MONOTONIC.
    The offset is used to convert from clock domain of
    camera to display.
    Added following set prop to switch between camera time sources.
    
    	persist.camera.time.monotonic <value>
    Default value is 1.
    value 0 indicates Boot time from camera.
    value 1 indicates Monotonic time from camera.
    
    Change-Id: I733aa17e89ccaa7f5ea6d2eae5e1cd6428c81a85
    CRs-Fixed: 1005906

--- camera/QCamera2/HAL/QCamera2HWI.h
+++ camera/QCamera2/HAL/QCamera2HWI.h
@@ -384,6 +384,7 @@ private:
     QCameraExif *getExifData();
     cam_sensor_t getSensorType();
     bool isLowPowerMode();
+    nsecs_t getBootToMonoTimeOffset();
 
     int32_t processAutoFocusEvent(cam_auto_focus_data_t &focus_data);
     int32_t processZoomEvent(cam_crop_data_t &crop_info);
@@ -766,6 +767,10 @@ private:
     bool m_bNeedRestart;
     Mutex mMapLock;
     Condition mMapCond;
+    
+    //The offset between BOOTTIME and MONOTONIC timestamps
+    nsecs_t mBootToMonoTimestampOffset;
+    
     // Count to determine the number of preview frames ignored for displaying.
     uint8_t mIgnoredPreviewCount;
 };
