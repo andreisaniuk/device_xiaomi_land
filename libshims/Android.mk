yu-community-os/android_vendor_volte

volte: Add IMS shim

libshims/Android.mk
@@ -80,3 +80,12 @@ LOCAL_C_INCLUDES += \
 LOCAL_MODULE:= libshims_camera
 
 include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := ims/MediaBuffer.c
+LOCAL_SHARED_LIBRARIES := libstagefright_foundation
+LOCAL_MODULE := libshims_ims
+LOCAL_MODULE_TAGS := optional
+
+include $(BUILD_SHARED_LIBRARY)

andreisaniuk/device_xiaomi_land

libshims/Android.mk
@@ -65,5 +65,14 @@ LOCAL_C_INCLUDES += \
 
 LOCAL_MODULE:= libshim_ims_camera
 LOCAL_MODULE_TAGS := optional
-LOCAL_32_BIT_ONLY := true
+
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := ims/MediaBuffer.c
+LOCAL_SHARED_LIBRARIES := libstagefright_foundation
+LOCAL_MODULE := libshim_ims
+LOCAL_MODULE_TAGS := optional
+
 include $(BUILD_SHARED_LIBRARY)